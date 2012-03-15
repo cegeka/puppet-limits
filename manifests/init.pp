3be491d0d79e74bbf2c8f440e9a8d286c354938e
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-03-15 11:43:21 +0100

    Initial repository layout for puppet module limits

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: limits
+#
+# This module manages limits
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class limits {
+
+
+}
---
+++ spec/classes/limits_spec.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe 'limits' do
+  it { should contain_class 'limits' }
+end
