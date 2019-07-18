a2ef187518036dc89e4757575f10307b2aaaf204
andreias <andrei.asan@cegeka.com>
2019-07-18 18:28:52 +0300

    puppet5 modifications

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -5,12 +5,17 @@ define limits::conf($domain, $type, $item, $value) {
   $key = "$domain/$type/$item"
   $context = '/files/etc/security/limits.conf'
 
+  $lens =  'Limits.lns'
+  $incl =  '/etc/security/limits.conf'
+
   $path_item = "domain[. = \"$domain\"][type = \"$type\" and item = \"$item\"]"
   $path_exact = "domain[. = \"$domain\"][type = \"$type\" and item = \"$item\" and value = \"$value\"]"
   $path_other = "domain[. = \"$domain\"][type = \"$type\" and item = \"$item\" and value != \"$value\"]"
 
   augeas { "limits.conf/$key/eof":
     context => $context,
+    lens => $lens,
+    incl => $incl,
     onlyif  => 'match #comment[. =~ regexp("End of file")] size > 0',
     changes => 'rm #comment[. =~ regexp("End of file")]',
     require => Class['limits::packages'],
@@ -18,6 +23,8 @@ define limits::conf($domain, $type, $item, $value) {
 
   augeas { "limits.conf/$key/rm":
     context => $context,
+    lens => $lens,
+    incl => $incl,
     onlyif  => "match $path_other size > 0",
     changes => "rm $path_item",
     before  => Augeas["limits.conf/$key/add"],
@@ -26,6 +33,8 @@ define limits::conf($domain, $type, $item, $value) {
 
   augeas { "limits.conf/$key/add":
     context => $context,
+    lens => $lens,
+    incl => $incl,
     onlyif  => "match $path_exact size == 0",
     changes => [
       "set domain[last()+1] $domain",
