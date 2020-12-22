592ee6da5ad1dcf231fa79c72521f7706709e3d5
Fabian Dammekens <fabian.dammekens@cegeka.be>
2020-12-22 20:36:35 +0100

    [TOMCAT9] Renta example host svrentdweb91.intra.cegeka.be

--- manifests/packages/redhat.pp
+++ manifests/packages/redhat.pp
@@ -1,7 +1,19 @@
 class limits::packages::redhat {
 
+  case $::os[release][major] {
+    '8': {
+      $repo_require = Dnf::Module['ruby']
+      ensure_resource('dnf::module','ruby',{'ensure'=>'present','stream'=>'2.5'})
+    }
+    default: {
+      $repo_require = Yumrepo['os']
+      Cegeka_yum::Repo <| title == 'os' |>
+    }
+  }
+
   package { ['augeas', 'augeas-libs', 'ruby-augeas'] :
-    ensure => present,
+    ensure  => present,
+    require => $repo_require
   }
 
 }
