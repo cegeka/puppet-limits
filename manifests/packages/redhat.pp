b6bcfee4ebc100fc15b3f8ade4fafcb5658b7e4c
Radu Catalin Andrei <radu-catalin.andrei@cegeka.com>
2025-08-27 14:51:30 +0300

    [LIMITS] Fix ruby duplicate declarations

--- manifests/packages/redhat.pp
+++ manifests/packages/redhat.pp
@@ -1,9 +1,18 @@
 class limits::packages::redhat {
+  $virtual_modules = lookup('profile::iac::repository_management::modules::virtual', Array, 'first', [])
+  $ruby_module_present = !empty($virtual_modules.filter |$module| { $module =~ /^ruby/ })
 
   case $::os[release][major] {
     '8': {
-      $repo_require = Dnf::Module['ruby']
-      ensure_resource('dnf::module','ruby',{'ensure'=>'present','stream'=>'2.5'})
+      # In certain cases a ruby module stream is already added by the repository_management::modules::virtual profile
+      # This leads to a duplicate declaration of the ruby module when used together with the limits profile
+      # We will first check if a ruby module stream is already defined, and only create this resource if it is absent
+      if ! $ruby_module_present {
+        $repo_require = Dnf::Module['ruby']
+        ensure_resource('dnf::module','ruby', { 'ensure' => 'present', 'stream' => '2.5' })
+      } else {
+      $repo_require = undef
+      }
     }
     default: {
       $repo_require = Yumrepo['os']
